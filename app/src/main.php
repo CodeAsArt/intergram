9dd2725c8a3f245ffe74085d4e0583bffa016fe7
Vojtech Sedlacek <codeasart@gmail.com>
2017-08-09 02:18:36 +0200

    Added simple response to user

--- app/src/main.php
+++ app/src/main.php
@@ -3,7 +3,11 @@
 $app->get('/', function () use ($app) {
     /** @var \Intergram\Parser\ServiceParser $parser */
     $parser = $app['intergram.parser'];
-    
+
     $parser->parse('./../data/hlaseni.t');
 
+//    $app['intergram.logger']->printLogs();
+    return 'Input file have been successfully parsed and validated.'
+        .' To see logs uncomment previous line (but will be prepared that it will eat your memory :)).'
+        .' However loging can be easily changed to log into file, database, wherever you would like.';
 });
